5449209bfe5585a199638e04cf2b052481556262
Dylan Knuth <dknuth02@aurora.edu>
2019-05-08 19:14:40 -0500

    V 1.01
    Allows for more than one hand

--- Blackjack/Blackjack/Driver.cs
+++ Blackjack/Blackjack/Driver.cs
@@ -23,20 +23,21 @@ namespace Blackjack
 
             HandOfCards cpuHand = new HandOfCards(gameDeck);
 
-            Console.WriteLine("\n&&&&&&&&&&&&&&&&&&&&&&&&&Dealers&&&&&&&&&&&&&&&&&&&&\n\n");
+            Console.WriteLine("\n*****************Dealers*******************\n\n");
             cpuHand.showDealerHand();
-            Console.WriteLine("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&n\n");
+            Console.WriteLine("\n*******************************************\n");
             int dealerCount = cpuHand.handValue();
 
             HandOfCards playerHand = new HandOfCards(gameDeck);
-            Console.WriteLine("\n###################YOURS########################\n");
+            Console.WriteLine("\n**********YOURS**********\n");
             playerHand.showHand();
             int playerHandValue = playerHand.handValue();
-            Console.WriteLine($"\nYour Hand Value: {playerHandValue}\n###################################################\n\n");
+            Console.WriteLine($"\nYour Hand Value: {playerHandValue}\n******************************\n\n");
 
             // player option : hit, stay, double
-            Console.WriteLine($"Your Hand Value: {playerHandValue}\nHIT(h) STAY(s) DOUBLE(d)??");
+            Console.WriteLine($"\nHIT(h) STAY(s) DOUBLE(d)??");
             string option = Console.ReadLine();
+            string keepGoing ="y";
 
             do
             {
@@ -44,30 +45,37 @@ namespace Blackjack
                 {
                     PlayingCard pc = gameDeck.dealCard();
                     playerHand.Add(pc);
-                    cpuHand.showDealerHand();
-                    Console.WriteLine("\n###################################################");
 
+
+                    Console.WriteLine("\n************************Dealers************************\n\n");
+                    cpuHand.showDealerHand();
+                    dealerCount = cpuHand.handValue();
+                    Console.WriteLine($"\nDealer Hand Value: {dealerCount}\n************************************************************\n\n");
+                    dealerCount = cpuHand.handValue();
+
+
+                    Console.WriteLine("\n************************YOURS************************\n");
                     playerHand.showHand();
                     playerHandValue = playerHand.handValue();
-                    // hit again?
-                    Console.WriteLine($"Your Hand Value: {playerHandValue}\nHIT(h) STAY(s)\n");
+                    Console.WriteLine($"\nYour Hand Value: {playerHandValue}\n************************************************************\n\n");
+                    Console.WriteLine($"\nHIT(h) STAY(s)\n");
                 }
 
                 else if (option == "s")
                 {
                     PlayingCard pc = gameDeck.dealCard();
 
-                    Console.WriteLine("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&Dealers&&&&&&&&&&&&&&&&&&&&&&&&n");
+                    Console.WriteLine("\n************Dealers************************\n");
                     cpuHand.showHand();
                     dealerCount = cpuHand.handValue();
-                    Console.WriteLine($"\nDealer Hand Value: {dealerCount}\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%n\n");
+                    Console.WriteLine($"\nDealer Hand Value: {dealerCount}\n************************************************************\n\n");
 
 
 
-                    Console.WriteLine("\n###################YOURS########################\n");
+                    Console.WriteLine("\n************YOURS************************\n");
                     playerHand.showHand();
                     playerHandValue = playerHand.handValue();
-                    Console.WriteLine($"\nYour Hand Value: {playerHandValue}\n###################################################\n\n");
+                    Console.WriteLine($"\nYour Hand Value: {playerHandValue}\n************************************************************\n\n");
 
 
 
@@ -75,10 +83,10 @@ namespace Blackjack
                     {
                         pc = gameDeck.dealCard();
                         cpuHand.Add(pc);
-                        Console.WriteLine("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&Dealers&&&&&&&&&&&&&&&&&&&&&&&&&&&&n");
+                        Console.WriteLine("\n************************Dealers************************\n");
                         cpuHand.showHand();
                         dealerCount = cpuHand.handValue();
-                        Console.WriteLine($"\nDealer Hand Value: {dealerCount}\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&n\n");
+                        Console.WriteLine($"\nDealer Hand Value: {dealerCount}\n************************************************************\n\n");
 
                     }
                     Console.WriteLine($"\nHIT(h) STAY(s)\n");
@@ -90,32 +98,25 @@ namespace Blackjack
                 {
                     PlayingCard pc = gameDeck.dealCard();
                     playerHand.Add(pc);
-                    Console.WriteLine("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&Dealers&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&n");
+                    Console.WriteLine("\n************Dealers************************\n");
                     cpuHand.showHand();
                     dealerCount = cpuHand.handValue();
-                    Console.WriteLine($"\nDealer Hand Value: {dealerCount}\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&n\n");
+                    Console.WriteLine($"\nDealer Hand Value: {dealerCount}\n************************************************************\n\n");
 
-                    Console.WriteLine("\n###################YOURS########################\n");
+                    Console.WriteLine("\n************YOURS************************\n");
                     playerHand.showHand();
                     playerHandValue = playerHand.handValue();
-                    Console.WriteLine($"\nYour Hand Value: {playerHandValue}\n###################################################\n\n");
+                    Console.WriteLine($"\nYour Hand Value: {playerHandValue}\n************************************************************\n\n");
 
-
-                    // playerHandValue = playerHand.handValue();
-                    //dealerCount = cpuHand.handValue();
-
-                    // Console.WriteLine($"Dealer value{dealerCount}\nYour Hand Value: {playerHandValue}");
-
-
-
+                                       
                     while (playerHandValue < 22 && dealerCount < 17)
                     {
                         pc = gameDeck.dealCard();
                         cpuHand.Add(pc);
-                        Console.WriteLine("\n###################Dealers########################\n");
+                        Console.WriteLine("\n************************Dealers************************\n");
                         cpuHand.showHand();
                         dealerCount = cpuHand.handValue();
-                        Console.WriteLine($"\nDealer Hand Value: {dealerCount}\n###################################################\n\n");
+                        Console.WriteLine($"\nDealer Hand Value: {dealerCount}\n************************************************************\n\n");
 
                     }
 
@@ -125,9 +126,30 @@ namespace Blackjack
                 }
 
                 winner(playerHandValue, dealerCount);
-                Console.WriteLine($"\nquit?(q)");
-                option = Console.ReadLine();
-            } while (option != "q");
+                Console.WriteLine($"\nKeep going(y)? or quit(q)?");
+                keepGoing = Console.ReadLine();
+
+                if (keepGoing == "y")
+                {
+                    cpuHand = new HandOfCards(gameDeck);
+
+                    Console.WriteLine("\n************************Dealers************************\n\n");
+                    cpuHand.showDealerHand();
+                    Console.WriteLine("\n************************************************************\n\n");
+                    dealerCount = cpuHand.handValue();
+
+                    playerHand = new HandOfCards(gameDeck);
+                    Console.WriteLine("\n************************YOURS************************\n");
+                    playerHand.showHand();
+                    playerHandValue = playerHand.handValue();
+                    Console.WriteLine($"\nYour Hand Value: {playerHandValue}\n************************************************************\n\n");
+
+                    // player option : hit, stay, double
+                    Console.WriteLine($"\nHIT(h) STAY(s) DOUBLE(d)??");
+                    option = Console.ReadLine();
+                }
+                //option = Console.ReadLine();
+            } while (keepGoing == "y");
         }
 
 
